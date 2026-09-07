ad2d8820818f2e6975a8152abce2bc82b60814b8
WyattAu <wyatt_au@protonmail.com>
2026-09-07 12:34:08 +0100

    chore: untrack fuzz build artifacts

--- fuzz/target/debug/build/libfuzzer-sys-db9b854f874fd992/build_script_build-db9b854f874fd992.d
+++
@@ -1,5 +0,0 @@
-/home/wyatt/dev/src/github.com/WyattAu/validkit/fuzz/target/debug/build/libfuzzer-sys-db9b854f874fd992/build_script_build-db9b854f874fd992.d: /home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.13/build.rs
-
-/home/wyatt/dev/src/github.com/WyattAu/validkit/fuzz/target/debug/build/libfuzzer-sys-db9b854f874fd992/build_script_build-db9b854f874fd992: /home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.13/build.rs
-
-/home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.13/build.rs:
--- fuzz/target/debug/build/thiserror-f6aaf0f709ceb8f5/build_script_build-f6aaf0f709ceb8f5.d
+++
@@ -1,5 +0,0 @@
-/home/wyatt/dev/src/github.com/WyattAu/validkit/fuzz/target/debug/build/thiserror-f6aaf0f709ceb8f5/build_script_build-f6aaf0f709ceb8f5.d: /home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.20/build.rs
-
-/home/wyatt/dev/src/github.com/WyattAu/validkit/fuzz/target/debug/build/thiserror-f6aaf0f709ceb8f5/build_script_build-f6aaf0f709ceb8f5: /home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.20/build.rs
-
-/home/wyatt/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.20/build.rs:
